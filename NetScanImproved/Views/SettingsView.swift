e6091babefa792e27d716566b631c78e519e7c04
Martin Martinez <mtz.martin@proton.me>
2024-04-04 15:36:05 -0600

    Basic view for my settings view

--- NetScanImproved/Views/SettingsView.swift
+++ NetScanImproved/Views/SettingsView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var ipServer: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Section {
+                TextField("IP address", text: $ipServer)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            } header: {
+                HStack {
+                    Text("IP address of the server")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+            } footer: {
+                HStack {
+                    Text("Available at my GitHub (martinolidev/api)")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                    Spacer()
+                }
+            }
+        }
+        .padding()
+        .navigationTitle("Settings")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    SettingsView()
+    ContentView()
 }
