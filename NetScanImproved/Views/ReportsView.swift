95a71c0ac48a43a2e454c3d698e79a0f774d8470
Martin Martinez <mtz.martin@proton.me>
2024-04-04 15:49:00 -0600

    A basic view for my reports, I will add some other logic

--- NetScanImproved/Views/ReportsView.swift
+++ NetScanImproved/Views/ReportsView.swift
@@ -6,13 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ReportsView: View {
+    @Environment(\.modelContext) private var context
+    @Query private var reports: [SaveReport]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if reports.isEmpty {
+                VStack {
+                    Image(systemName: "plus.square.fill.on.square.fill")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.accentColor)
+                    
+                    Text("Scan a device and then click save!")
+                        .bold()
+                        .font(.headline)
+                }
+            }
+        }
+        .padding()
+        .navigationTitle("My reports")
     }
 }
 
 #Preview {
-    ReportsView()
+    ContentView()
 }
